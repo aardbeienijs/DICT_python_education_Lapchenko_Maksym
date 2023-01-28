3c6c690a41fa7d7af0c0a5541a5f8515ceadf8b1
aardbeienijs <m.m.lapchenko@student.khai.edu>
2023-01-28 10:50:26 +0100

    s ba90f8ec f 6b053ec0 ChatBot {5-th} stage
    ChatBot {2-nd} stage
    
    s ba90f8ec
    f 6b053ec0
    
    ChatBot {2-nd} stage
    s 6b053ec0
    f 18fb0974

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -2,16 +2,16 @@ print("Hello! My name is DICT_BOT")
 print("I was created in 2000")
 print("Please, remind me your name")
 a = (input())
-b = 'What a great name you have,'
-print( b + a )
+b = ('What a great name you have,')
+print((a) (b))
 print("Let me guess your age")
 print("Enter remainders of dividing your age by 3,5 and 7")
 remainder3 = int(input())
 remainder5 = int(input())
 remainder7 = int(input())
 age = ((remainder3*70 + remainder5*21+ remainder7*15) % 105)
-print( "Your age is" ,age,  ';' "thats a good time to start programming")
-print('Now I will prove to you that I can count to any number you want.')
+print( "Your age is good age for start programming ")
+print('i can prove to you that I can count to any number you want.')
 while True:
     user_print = int(input())
     if user_print <= 10:
@@ -79,3 +79,21 @@ elif user_print == 9:
     print('8!')
     print('9!')
     print('Completed, have a nice day')
+print('Lets test your programming knowledge')
+print('Why do we use methods?')
+print('1. To repeat a statement multiple times.')
+print('2. To decompose a program into several small subroutines.')
+print('3. To determine the execution time of a program.')
+print('4. To interrupt the execution of a program.')
+while True:
+    user_print = int(input())
+    if user_print == 2:
+      print('Completed, have a nice day!')
+      print('Congratulations, have a nice day!')
+      break
+    elif user_print < 2:
+      print('Please, try again.')
+
+    elif user_print > 2:
+        print('Please, try again.')
+
